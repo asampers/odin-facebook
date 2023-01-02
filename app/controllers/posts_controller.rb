9f13dd48d5f2cd0a5cad02e455d4aee82340ddc0
nanarama <annasampers@gmail.com>
2023-01-02 15:58:40 -0600

    Add Post model referencing User

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,30 @@
+class PostsController < ApplicationController
+before_action :authenticate_user!, only: [:new, :create]
+
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+
+    if @post.save     
+      redirect_to posts_path
+    else  
+      render :new, status: :unprocessable_entity
+    end     
+  end
+
+  def index
+    @posts = Post.all
+  end
+
+  private 
+  def post_params
+    params.require(:post).permit(:body)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_02_201524) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_02_213316) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string "body"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username", default: "", null: false
@@ -28,4 +36,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_02_201524) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
