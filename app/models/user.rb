707b8972e8807051e1e823365cd833b4c2cb9e04
nanarama <annasampers@gmail.com>
2023-01-03 13:30:55 -0600

    Fix sign up to allow for username or email

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,24 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :posts       
+  has_many :posts   
+
+  validates :username, presence: true, uniqueness: true
+
+  attr_writer :login
+
+  def login
+    @login || username || email
+  end
+
+  private
+
+  def self.find_for_database_authentication(warden_condition)
+    conditions = warden_condition.dup
+    if(login = conditions.delete(:login))
+      where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { value: login.downcase }]).first
+    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+      where(conditions.to_h).first
+    end
+  end    
 end
