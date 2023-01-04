c8a67e286c778ed14611720de751bd4ea1e70854
nanarama <annasampers@gmail.com>
2023-01-04 11:48:46 -0600

    Add User controller with index and show pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
+  
   root to: "posts#index"
 
+  resources :users, only: %i[show index]
   resources :posts
 end
