0ec4830d38fdb447d3aeea927e176bd070727d50
nanarama <annasampers@gmail.com>
2023-01-10 17:24:25 -0600

    Added styling and links to header nav

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  get 'friendships/index'
-  get 'friendships/show'
-  get 'friendships/new'
-  get 'friendships/create'
-  get 'friendships/destroy'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
