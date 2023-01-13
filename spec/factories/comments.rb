90d69c4faf208fa4436e9f0513abeb6045ce8460
nanarama <annasampers@gmail.com>
2023-01-13 13:22:30 -0600

    Add body to comments

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :comment do
-    user { nil }
+    user { :jane }
     post { nil }
     parent_id { 1 }
   end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:jane) { create(:user, :jane) }
+  let!(:john)  { create(:user, :john) }
+  let!(:john_post) { john.posts.build("This is John's post.") }
+  let!(:jane_comment) { john_post.build(user_id: jane.id, body: "Great post, John!")}
+  let!(:john_comment) { jane_comment.comments.build(user_id: john, body: "Nice comment, Jane")}
+  
 end
