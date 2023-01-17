b59a525cda961a435019028d4bd51192be45dea8
nanarama <annasampers@gmail.com>
2023-01-17 15:26:35 -0600

    Cleanup

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
-  has_many :comments, dependent: :destroy 
+  has_many :comments, dependent: :destroy, strict_loading: true 
 
   validates :body, :user_id, presence: true
 end
