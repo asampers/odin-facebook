7de9dcc5e82f7e213757bef19dc945eac321e2ab
nanarama <annasampers@gmail.com>
2023-01-23 17:35:49 -0600

    Cleanup and add notifications count to header

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,16 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
+  let!(:example) { create(:user, :faker) }
+
+  before do
+    login_as(example)
+  end 
+
   describe "GET /index" do
     it "returns http success" do
-      get "/users/index"
+      get "/users"
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET /show" do
     it "returns http success" do
-      get "/users/show"
+      get "/users/#{example.id}"
       expect(response).to have_http_status(:success)
     end
   end
