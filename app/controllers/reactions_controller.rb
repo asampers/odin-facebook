f9420e18e4e65c6b7e2aedf47945a3d77fd9c24b
nanarama <annasampers@gmail.com>
2023-01-30 15:14:57 -0600

    Add reactions_controller

---
+++ app/controllers/reactions_controller.rb
@@ -0,0 +1,23 @@
+class ReactionsController < ApplicationController
+  def create
+    @reaction = current_user.reactions.create(reaction_params)
+
+    if !@reaction.save
+      flash[:alert] = @reaction.errors.full_messages.to_sentence
+    end 
+
+    redirect_to request.referrer   
+  end
+
+  def destroy
+    @reaction = current_user.reactions.find(params[:id])
+    @reaction.destroy
+    redirect_to request.referrer
+  end
+
+  private
+
+  def reaction_params
+    params.require(:reaction).permit(:reactable_id, :reactable_type)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  get 'reactions/create'
+  get 'reactions/destroy'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   root to: "posts#index"
 
+  resources :reactions, only: %i[create destroy]
   resources :friendships
   resources :users do
     resources :profiles
