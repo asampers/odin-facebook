71c3b1cd3afb051c6953eacedff8154b6f9df0c0
nanarama <annasampers@gmail.com>
2023-01-31 13:25:22 -0600

    Fiddling with buttons on post view

--- app/controllers/reactions_controller.rb
+++ app/controllers/reactions_controller.rb
@@ -1,11 +1,12 @@
 class ReactionsController < ApplicationController
   def create
     @reaction = current_user.reactions.create(reaction_params)
-
+    user = @reaction.reactable.user 
     if !@reaction.save
       flash[:alert] = @reaction.errors.full_messages.to_sentence
     end 
 
+    notify(user, @reaction)
     redirect_to request.referrer   
   end
 
@@ -16,6 +17,12 @@ class ReactionsController < ApplicationController
   end
 
   private
+
+  def notify(user, reaction)
+    return if current_user == user 
+
+    user.notifications.create(notifiable: reaction)
+  end
 
   def reaction_params
     params.require(:reaction).permit(:user_id, :reactable_id, :reactable_type)
