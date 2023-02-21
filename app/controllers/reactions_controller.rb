a5366be7fc498d6f99a7f306300c7fad6b170872
nanarama <annasampers@gmail.com>
2023-02-21 15:25:23 -0600

    Use turbo_stream for create and destroy actions with reaction

--- app/controllers/reactions_controller.rb
+++ app/controllers/reactions_controller.rb
@@ -1,28 +1,40 @@
 class ReactionsController < ApplicationController
   before_action :set_post, only: %i[index]
-  
+  before_action :set_reaction, only: %i[destroy]
+
   def create
     @reaction = current_user.reactions.create(reaction_params)
     user = @reaction.reactable.user 
     
-    if !@reaction.save
+    if @reaction.save
+      notify(user, @reaction)
+      respond_to do |format|
+        format.html { redirect_to request.referrer }
+        format.turbo_stream
+      end 
+    else 
       flash[:alert] = @reaction.errors.full_messages.to_sentence
-    end 
-    
-    notify(user, @reaction)
-    redirect_to request.referrer   
+      redirect_to request.referrer  
+    end   
   end
 
   def destroy
-    @reaction = current_user.reactions.find(params[:id])
     @reaction.destroy
-    redirect_to request.referrer
+
+    respond_to do |format|
+      format.html { redirect_to request.referrer }
+      format.turbo_stream
+    end  
   end
 
   private
 
   def set_post
     @post = Post.find(params[:post_id])
+  end
+
+  def set_reaction
+    @reaction = Reaction.find(params[:id])
   end
   
   def notify(user, reaction)
