64bf8bd12a2101b26fa464b44271cbb5e640ca25
nanarama <annasampers@gmail.com>
2023-02-22 16:58:40 -0600

    Refactor new notifications count

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,11 +1,13 @@
 class Notification < ApplicationRecord
   include OrderableByTimestamp
-  
+
   belongs_to :user
   belongs_to :sender, class_name: 'User'
   belongs_to :notifiable, polymorphic: true
 
   validates :user_id, uniqueness: { scope: %i[notifiable_id notifiable_type] }
+
+  scope :num_of_new, -> { where(was_read: false).count }
 
   def message
     notifiable.message
