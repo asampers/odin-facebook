b66197622d312f264761c34db6086eb2ede973ca
nanarama <annasampers@gmail.com>
2023-02-22 17:28:34 -0600

    Fix creating new post from root_path

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,7 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.includes(:user).by_recently_created
+    @post = current_user.posts.build
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  root to: "posts#index"
+  root "posts#index"
 
   resources :reactions, only: %i[create destroy]
   resources :friendships
