bb56f3aa3e5e69e3879fca4a5a161e45a578613a
nanarama <annasampers@gmail.com>
2023-02-22 16:27:44 -0600

    Clean up usersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,9 @@ class UsersController < ApplicationController
 
   def show
     @user ||= User.find(params[:id])
-    @accepted_friends = @user.friendships.includes(:user).accepted.map(&:user) + @user.friendships.includes(:friend).accepted.map(&:friend)
-    @pending_friends = @user.friendships.includes(:user).pending.map(&:user) + @user.friendships.includes(:friend).pending.map(&:friend)
+    @accepted_friends = User.find(@user.friendships.accepted.pluck(:user_id, :friend_id))
+    @pending_friends = User.find(@user.friendships.pending.pluck(:user_id, :friend_id))
+    @num_of_friends = @accepted_friends.reject{|user| user == @user}.size
   end
 
   def notifications
