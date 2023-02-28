f8e2aded0524f6c1fb80601d921ae88c75f354a0
nanarama <annasampers@gmail.com>
2023-02-28 16:33:26 -0600

    Add popular posts view

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,6 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy 
   has_many :reactions, as: :reactable, dependent: :destroy 
   validates :body, :user_id, presence: true
+
+  scope :popular, -> { order(reactions_count: :desc).limit(5)}
 end
