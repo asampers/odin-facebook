97f43175c7ddc15f309c1cbc719bcc0bbf8b5ed1
nanarama <annasampers@gmail.com>
2023-03-27 10:30:00 -0500

    Update welcome mailer subject line

--- app/mailers/welcome_mailer.rb
+++ app/mailers/welcome_mailer.rb
@@ -4,6 +4,6 @@ class WelcomeMailer < ApplicationMailer
   def welcome_email(user)
     @user = user
     @url = 'https://friendbook.fly.dev/'
-    mail(to: @user.email, subject: 'Welcome to Sampers Social Media.')
+    mail(to: @user.email, subject: 'Welcome to Friend Book.')
   end
 end
